7784ca42e942e163ad2f0fb0e42627ba5fb8699b
Richy <113453694+richyrach@users.noreply.github.com>
2025-09-20 19:46:59 +0330

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,6 +1,4 @@
-module.exports = async (req, res) => {
-  res.setHeader("Set-Cookie", "sr_session=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax");
-  res.statusCode = 302;
-  res.setHeader("Location", "/?logged=0");
-  res.end();
-};
+res.setHeader("Set-Cookie", [
+  "sr_session=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax",
+  "sr_auth=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax"
+]);
